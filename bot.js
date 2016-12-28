661244f8d557842acb43df6d718ea91fabfcf349
brad217 <braddaugherty8@gmail.com>
2016-12-28 16:25:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/; botRegex4d = /^\/4th/; botRegexDL = /^\/rules/; botRegexTw = /^\/twitch/i; botRegexP = /^\/PDL/i; 
+      botRegex = /^\/cool guy/; botRegex4d = /^\/4th/; botRegexDL = /^\/rules/; botRegexTw = /^\/twitch/i; botRegexP = /^\/PDL/i;
  
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
