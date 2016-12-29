09140c7bcbcf4ed08c5ed768f18a3093e5b511bd
brad217 <braddaugherty8@gmail.com>
2016-12-29 16:37:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,6 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/; botRegexDa = /^\/dae/; botRegex4d = /^\/4th/; botRegexDon = /^\/correct/; 
-  var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
-                ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
-                "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
@@ -16,17 +13,12 @@ function respond() {
   }
    else if(request.text && botRegexDa.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://imgur.com/HZNJtD4");
+    postMessage("https://i.groupme.com/540x960.png.d946e263ad1e427f9cf739bcb626f78b");
     this.res.end();
    }
-  else if(request.text && botRegexDL.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://amfl.freeforums.org/league-rules-t3.html");
-    this.res.end();
-  }
   else if(request.text && botRegexDon.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://media0.giphy.com/media/26tknCqiJrBQG6bxC/200_s.gif");
+    postMessage("https://i.groupme.com/480x446.gif.86ec7649965f4ce4881eaa66814b6a9f");
     this.res.end();
   }
   else {
