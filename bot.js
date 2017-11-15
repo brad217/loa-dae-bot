3b9c1bcf672360434a6682808961479672f17dd0
brad217 <braddaugherty8@gmail.com>
2017-11-14 20:49:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ function respond() {
   }
   else if(request.text && botRegexDig.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://imgur.com/a/ju2e5");
+    postMessage("https://imgur.com/a/uqZmv");
     this.res.end();
   }
   else if(request.text && botRegexTrde.test(request.text)) {
